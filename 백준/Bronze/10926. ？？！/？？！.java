521c02afe2250ad4d58c35fade2375f552a9dfa7
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2024-12-31 00:55:41 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String  a;
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextLine();
+
+        System.out.println(a+"??!");
+
+
+    }
+}
