c2ff94a6e3359ee83ba6304bda2739ef8e397653
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2024-12-31 01:40:15 +0900

    [Bronze V] Title: 윤년, Time: 184 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+
+
+        if (a % 4 == 0  && a % 100 != 0 || a % 400 == 0 ) {
+            System.out.println("1");
+        }
+        else {
+            System.out.println("0");
+        }
+
+        sc.close();
+    }
+}
