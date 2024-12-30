d35218395f25ae5956385f9e13f1193762390ba7
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2024-12-31 01:20:40 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long A;
+        long B;
+        long C;
+        Scanner sc = new Scanner(System.in);
+
+        A = sc.nextLong();
+        B = sc.nextLong();
+        C = sc.nextLong();
+
+        System.out.println(A + B + C);
+
+    }
+}
