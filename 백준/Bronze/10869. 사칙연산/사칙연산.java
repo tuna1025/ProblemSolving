4d3c4b81358fbd07d8913ad2d14c05e948f51209
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2024-12-31 00:51:36 +0900

    [Bronze V] Title: 사칙연산, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+
+        System.out.printf("%d\n", a + b);
+        System.out.printf("%d\n", a - b);
+        System.out.printf("%d\n", a * b);
+        System.out.printf("%d\n", a / b);
+        System.out.printf("%d\n", a % b);
+
+    }
+}
