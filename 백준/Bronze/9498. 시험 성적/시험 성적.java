99b2bc0633da17b7b7f1964b5743c274c90c35d2
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2024-12-31 01:31:47 +0900

    [Bronze V] Title: 시험 성적, Time: 176 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+
+
+        if (a >= 90) {
+            System.out.println("A");
+        }
+        else if (a >= 80) {
+            System.out.println("B");
+        }
+        else if (a >= 70) {
+            System.out.println("C");
+        }
+        else if (a >= 60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}
