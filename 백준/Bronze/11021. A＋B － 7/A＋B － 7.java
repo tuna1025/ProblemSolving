4db3747384cf9d8de807d54dbec8e57e3afa895f
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-01-01 03:59:38 +0900

    [Bronze V] Title: A+B - 7, Time: 144 ms, Memory: 16108 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수 읽기
+        for (int test = 1; test <= T; test++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            bw.write("Case #"+ test + ": " + (N + M) + "\n"); // 두 정수를 더한 결과를 출력
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
