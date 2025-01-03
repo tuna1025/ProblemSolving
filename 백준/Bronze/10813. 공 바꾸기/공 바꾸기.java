b517a3fd12d9dbb3889b96f1c0354cafaba096db
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-01-03 21:52:53 +0900

    [Bronze II] Title: 공 바꾸기, Time: 120 ms, Memory: 15840 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+        
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int temp = arr[S-1];
+            arr[S-1] = arr[E-1];
+            arr[E-1] = temp;
+        }
+        for (int baguni : arr)
+            System.out.print(baguni + " ");
+
+    }
+}
