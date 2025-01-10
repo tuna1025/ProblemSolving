cd001dee820693c8c3214c9d87886f323e12cde0
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-01-10 14:43:41 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] arr = new int[9][9];
+        int max = Integer.MIN_VALUE;
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i + 1;
+                    col = j + 1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row + " " + col);
+        br.close();
+    }
+}
