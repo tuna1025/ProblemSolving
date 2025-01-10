8a000b4ed6d9a3b97ea3e34de82ad8b463e9ff55
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-01-10 15:09:18 +0900

    [Bronze I] Title: 세로읽기, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[][] arr = new String[5][15];
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                if(line.charAt(j) != ' ') {
+                    arr[i][j] = line.charAt(j) + "";
+                }
+            }
+        }
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] != null) {
+                    System.out.print(arr[j][i]);
+                }
+            }
+        }
+        br.close();
+    }
+}
