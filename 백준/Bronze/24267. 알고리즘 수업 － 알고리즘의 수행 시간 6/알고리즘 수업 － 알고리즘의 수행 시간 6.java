292080cc95498b394914a157fc0d838ee57767a0
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-01-14 15:03:27 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       long n = sc.nextLong();
+       System.out.println((n-2)*(n-1)*n/6);
+       System.out.println(3);
+
+    }
+}
