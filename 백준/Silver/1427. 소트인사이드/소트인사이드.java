cd2f0cd53f6750eebcd447e0a42cd3b31b7613d3
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-01-18 17:12:05 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -4,7 +4,6 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         int[] arr = new int[11]; //주어진 수의 범위 1 ~ 10000, 인덱스와 수를 매칭시키기 위해 1추가로 잡음
         String s = br.readLine();
@@ -14,14 +13,14 @@ class Main {
 
         for (int i = arr.length-1; i >= 0; i--) {
             while (arr[i] != 0) {
-               sb.append(i);//문자열 누적
+                bw.write(i + "");
                 arr[i]--;
             }
         }
-        bw.write(sb.toString()); //한번에 출력, 메모리 관리에 좋은듯?
         // O(n) + 알파?, 1차원 배열 써서 O(n)이라고 해도 될듯
         bw.flush();
         bw.close();
         br.close();
     }
 }
+
