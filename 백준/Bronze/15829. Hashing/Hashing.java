078df0ba4de6e76d69079b09038e8688d980f129
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-02-04 01:45:29 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14368 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -11,7 +11,7 @@ class Main {
         for (int i = 0; i < t; i++) {
             b = b.add(BigInteger.valueOf(s.charAt(i)-96).multiply(BigInteger.valueOf(31).pow(i)));
         }// 'a'는 97, b에 알파벳의 해당하는 1~26사이의 값에 31의 (문자열길이)승만큼의 제곱수 곱해줌
-        System.out.println(b);
+        System.out.println(b.mod(BigInteger.valueOf(1234567891)));
     }
 }
 
