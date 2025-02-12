54a3f01cf890694ee65825ceeeeec16695968bfb
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-02-12 21:00:00 +0900

    [Bronze V] Title: 검증수, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        while (st.hasMoreTokens()) {
+            sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
+        }
+        System.out.println(sum%10);
+        br.close();
+    }
+}
