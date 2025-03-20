954e2aa08ca86441396b60126285779005834d9e
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-03-20 11:02:13 +0900

    [Bronze III] Title: TGN, Time: 96 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if(a < (b-c))  bw.write("advertise\n");
+            else if(a == (b-c)) bw.write("does not matter\n");
+            else bw.write("do not advertise\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
