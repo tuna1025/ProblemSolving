51f70f1226c11174afc6548cd325b814a1d55444
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-01 17:10:36 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}
