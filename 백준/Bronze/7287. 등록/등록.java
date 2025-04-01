573ee507e970f650f02b3f60937892fad766eaec
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-01 13:53:37 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*; 
+import java.util.*; 
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("241");
+        System.out.println("dlehddnjs444");
+    }
+}
