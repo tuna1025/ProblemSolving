8b495ef9e9e652c10d296ea64acdb1cf4abee774
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-01 17:20:23 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 188 ms, Memory: 18188 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.print(":fan::"+s+"::fan:\n");
+        System.out.println(":fan::fan::fan:");
+    }
+}
