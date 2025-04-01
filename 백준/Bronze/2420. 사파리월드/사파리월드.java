4aea5ae5d2ef4a9f2b1f26c59704bb1beb6a8e75
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-01 14:01:04 +0900

    [Bronze V] Title: 사파리월드, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+        BigInteger abs = n.subtract(m);
+        abs = abs.abs();
+        System.out.println(abs);
+    }
+}
