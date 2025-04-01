7ee3909471e29a7c1e7bb3a991ffae5009eca45e
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-01 17:15:08 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 100 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("고려대학교");
+    }
+}
