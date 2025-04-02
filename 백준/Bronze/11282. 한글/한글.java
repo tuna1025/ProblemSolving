69006b406b5262353c683b5794b4fbc59085802c
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-02 23:54:44 +0900

    [Bronze IV] Title: 한글, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char n = (char) (44031+Integer.parseInt(br.readLine()));
+        System.out.println(n);
+    }
+}
