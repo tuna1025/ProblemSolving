14e777d0b5bb1ef32f55a15a926d460bdb69cbab
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-03 01:14:51 +0900

    [Bronze IV] Title: 한글 2, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = br.readLine().charAt(0)-44031;
+        System.out.println(n);
+    }
+}
