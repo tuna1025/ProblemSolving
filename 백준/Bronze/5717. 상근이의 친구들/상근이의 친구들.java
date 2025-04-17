5795f3f0efa388d32289d13f37e7af73bdb15eba
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-04-17 22:38:11 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 96 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if(n == 0 && m == 0) break;
+            System.out.println(n + m);
+        }
+    }
+}
