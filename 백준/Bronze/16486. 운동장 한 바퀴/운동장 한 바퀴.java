37ccebe7c8f8d9d5f46482d2747c6027357586d9
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-05-12 15:59:20 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a*2 + 2 * b * 3.141592);
+    }
+
+}
