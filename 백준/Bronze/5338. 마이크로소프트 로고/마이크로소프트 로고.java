b46e5085ac0f8268ce3fc47231503a931a9c6bff
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-05-19 00:35:49 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 100 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(
+                "       _.-;;-._\n" +
+                        "'-..-'|   ||   |\n" +
+                        "'-..-'|_.-;;-._|\n" +
+                        "'-..-'|   ||   |\n" +
+                        "'-..-'|_.-''-._|"
+        );
+    }
+
+}
