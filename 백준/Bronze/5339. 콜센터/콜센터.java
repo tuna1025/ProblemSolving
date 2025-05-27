60fd0c8ea0853741792a3321cdfe0d56cda09a1a
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-05-27 23:59:49 +0900

    [Bronze V] Title: 콜센터, Time: 92 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.SortedMap;
+
+
+class Main{
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
