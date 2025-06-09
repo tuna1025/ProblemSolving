89abeccf58e46357ab3cd7b7aa1944d958147208
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-06-09 18:36:20 +0900

    [Bronze V] Title: Can you add this?, Time: 188 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a + b);
+        }
+
+    }
+}
