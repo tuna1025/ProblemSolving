b94e95bc3db3448793ee22c3e83ed3c3804e95ae
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-06-17 22:54:15 +0900

    [Bronze V] Title: Zadanie próbne, Time: 104 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            System.out.print(s.charAt(i));
+        }
+
+    }
+}
