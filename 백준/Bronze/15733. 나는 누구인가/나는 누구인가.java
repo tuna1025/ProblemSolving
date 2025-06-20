57e01b006a9dfb0da14a79d73310d2d99f3d97ca
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-06-21 03:34:44 +0900

    [Bronze V] Title: 나는 누구인가, Time: 96 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("I'm Sexy");
+    }
+}
