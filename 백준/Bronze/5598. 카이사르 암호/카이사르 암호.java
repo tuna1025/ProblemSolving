43b56d33384050cb0a71e2bb4e66c643b505c618
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-08-12 00:11:50 +0900

    [Bronze II] Title: 카이사르 암호, Time: 112 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/5598. 카이사르 암호/카이사르 암호.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        char[] alphabet = {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                'U', 'V', 'W', 'X', 'Y', 'Z'
+        };
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i++) {
+            int c = (s.charAt(i) - 'A' - 3 + 26) % 26;
+            sb.append(alphabet[c]);
+        }
+        System.out.println(sb);
+
+    }
+}
