e6a21410bb4ab42e4f57b9b5b46961fcafa1c36b
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-08-14 23:08:10 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int ans = Math.min(m, k) + Math.min(n-m, n-k);
+        System.out.println(ans);
+    }
+}
