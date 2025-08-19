8654a495291769bec3f429af970144075c795cd6
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-08-19 22:03:45 +0900

    [Bronze V] Title: Cupcake Party, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a*8 + b*3 - 28);
+    }
+}
