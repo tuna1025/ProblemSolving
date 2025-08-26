0cbd8f0f0a35f61a3b98702d7b37919869c230b1
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-08-27 00:48:16 +0900

    [Silver V] Title: 불사조, Time: 108 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/31780. 불사조/불사조.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println(x*(m+1));
+
+    }
+}
