df785bd52bc8da194719b7ad8eced76cf1605902
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-08-28 00:23:13 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 108 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] board = {{"11", "A B C D E F G H J L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A B C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A B C F G H L M"}};
+        System.out.println(board[n-1][0]);
+        System.out.println(board[n-1][1]);
+
+    }
+}
