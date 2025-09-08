7f4b97bfb16ca7abe31caa350d75d0062d00c2fb
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-09-08 23:46:42 +0900

    [Bronze V] Title: 자동완성, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        if(s.equals("n")||s.equals("N")) {
+            System.out.println("Naver D2");
+        }
+        else {
+            System.out.println("Naver Whale");
+        }
+    }
+}
