c2dfa3ce8e2af138a0b4cb03d935d56b6a745390
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-09-16 00:15:07 +0900

    [Bronze V] Title: Copier, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.printf("%d %d\n", n,n);
+        }
+    }
+}
