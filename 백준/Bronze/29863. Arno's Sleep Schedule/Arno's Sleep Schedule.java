9b52d7cfed4651008f9b6452c3bcdd96db7c332e
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-10-05 22:58:57 +0900

    [Bronze V] Title: Arno's Sleep Schedule, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/29863. Arno's Sleep Schedule/Arno's Sleep Schedule.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        if(a>3) System.out.println(24-a + b);
+        else System.out.println(b - a);
+
+
+    }
+}
