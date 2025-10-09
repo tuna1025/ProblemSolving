4181fe81484affe2fcc2e133e0a3e9ab26e2c053
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-10-10 00:20:30 +0900

    [Bronze V] Title: Java Warriors, Time: 176 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n*4000);
+
+    }
+}
