51950737909a4a577ab078eb3c514bd5ae75c8c4
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-10-16 23:34:55 +0900

    [Bronze V] Title: Adding Trouble, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        if(a+b == c){
+            System.out.println("correct!");
+        }
+        else{
+            System.out.println("wrong!");
+        }
+    }
+}
