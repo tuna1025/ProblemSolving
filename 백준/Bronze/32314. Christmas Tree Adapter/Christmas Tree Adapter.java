de3563b46b39839c065a809f97a8465d993d90ee
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-10-20 00:12:57 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        if(a <= (double) w/v) System.out.println(1);
+        else System.out.println(0);
+    }
+}
