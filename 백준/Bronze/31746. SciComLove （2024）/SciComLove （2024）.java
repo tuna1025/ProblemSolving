08b96134b19408d10b7bb2afa4d5257e9d6253d4
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-11-27 01:45:50 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 100 ms, Memory: 14084 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.println(a%2 == 0 ? "SciComLove" : "evoLmoCicS");
+    }
+}
