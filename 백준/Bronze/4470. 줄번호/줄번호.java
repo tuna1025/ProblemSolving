85c8e16bbec96a469d10b16b294e24b45960dda2
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2025-12-09 21:32:09 +0900

    [Bronze IV] Title: 줄번호, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%d. %s\n",i, br.readLine());
+        }
+    }
+}
