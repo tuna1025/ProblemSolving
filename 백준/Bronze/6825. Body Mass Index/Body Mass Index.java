88cbd569437d874b60845db39b6fcb21c79069f2
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2026-02-27 00:21:18 +0900

    [Bronze IV] Title: Body Mass Index, Time: 176 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Bronze/6825. Body Mass Index/Body Mass Index.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double weight = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        double bmi = weight / (height * height);
+
+        if (bmi >= 25.0) {
+            System.out.println("Overweight");
+        } else if (bmi >= 18.5) {
+            System.out.println("Normal weight");
+        } else {
+            System.out.println("Underweight");
+        }
+    }
+}
