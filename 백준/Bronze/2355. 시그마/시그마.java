a50b03262fb6ecd981d596f37283d732386e1e04
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2026-03-17 02:50:59 +0900

    [Bronze II] Title: 시그마, Time: 168 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2355. 시그마/시그마.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+
+        long n = Math.abs(A - B) + 1;
+        long sum = (A + B) * n / 2;
+
+        System.out.println(sum);
+    }
+}
