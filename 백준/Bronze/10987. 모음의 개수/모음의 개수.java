3a24e6aa55686b3acb96e976e9482ca78312bfc7
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2026-04-15 01:22:29 +0900

    [Bronze IV] Title: 모음의 개수, Time: 164 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
